fc56df709728ceb2eb85e977db3490fc7c707f69
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2024-08-22 22:28:39 -0300

    refactor: change server log emoji to rocket

--- apps/api/src/infra/http/server.ts
+++ apps/api/src/infra/http/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
 
 app.listen(3333, () => {
-	console.log(`🔌 HTTP server is running at ${app.server?.port}!`);
+	console.log(`🚀 HTTP server is running at ${app.server?.port}!`);
 });
